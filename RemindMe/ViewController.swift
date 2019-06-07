204cf35295b8b6202adcd4c7cc3162e40fc3ea6a
Yuchao CHEN <yuchao.chen@hotmail.com>
2019-06-07 10:57:08 +1000

    Initial Commit

---
+++ RemindMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemindMe
+//
+//  Created by Yuchao CHEN on 7/6/19.
+//  Copyright © 2019 Yuchao CHEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
